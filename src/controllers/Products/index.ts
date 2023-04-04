08d05f2470ae96e0f35fd18ffa9ed5b23b701c75
allancassio <allan.cassio1@gmail.com>
2023-04-03 22:10:48 -0300

    create controllers products

--- src/controllers/Products/index.ts
+++ src/controllers/Products/index.ts
@@ -1,3 +1,31 @@
 import { Request, Response } from "express";
 
-export const controllerUser = {};
+export const controllerProducts = {
+  create: (req: Request, res: Response) => {
+    const { body } = req;
+    return res.send("create");
+  },
+  getAll: (req: Request, res: Response) => {
+    return res.send("all");
+  },
+  findProductById: (req: Request, res: Response) => {
+    const { id } = req.params;
+    return res.send("findOne");
+  },
+  findProductByRecipientId: (req: Request, res: Response) => {
+    const { id } = req.params;
+    return res.send("findOne");
+  },
+  findProductBySendedId: (req: Request, res: Response) => {
+    const { id } = req.params;
+    return res.send("findOne");
+  },
+  update: (req: Request, res: Response) => {
+    const { id } = req.params;
+    return res.send("update");
+  },
+  delete: (req: Request, res: Response) => {
+    const { id } = req.params;
+    return res.send("delete");
+  },
+};
